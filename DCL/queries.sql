f74c583a87049700661835707c4b314c205a086c
Aryan Negi <negiaryan2803@gmail.com>
2025-01-09 10:47:25 +0530

    [Aryan] Add: Executing DCL commands using psql

--- DCL/queries.sql
+++ DCL/queries.sql
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-Q1 grant select on staff to postgres;
-GRANT
-create user Aryan;
-create user Aalekh;
-create user Aman;
-CREATE ROLE
-CREATE ROLE
-CREATE ROLE
-Q2 grant insert on staff to Aryan,Aalekh,Aman;
-GRANT
-Q3 revoke update on staff from Aryan;
-REVOKE
-Q4 grant all privileges on products to Aryan;
-GRANT
-Q5 revoke delete on products from public;
-REVOKE
-Q6 grant select,insert on departments to Aryan;
-=======
 List of databases
    Name    |  Owner   | Encoding | Locale Provider |      Collate       |       Ctype        | Locale | ICU Rules |   Access privileges   
 -----------+----------+----------+-----------------+--------------------+--------------------+--------+-----------+-----------------------
@@ -72,28 +53,11 @@ Q5. revoke delete on products from public
 REVOKE
 
 Q6. grant select , insert on departments to Aman;
->>>>>>> DCL
 GRANT
             List of relations
  Schema |    Name     | Type  |  Owner   
 --------+-------------+-------+----------
  public | departments | table | postgres
-<<<<<<< HEAD
- public | products    | table | postgres
- public | staff       | table | postgres
-(3 rows)
-
-Q7 revoke all privileges on departments from Aryan;
-REVOKE
-Q8 create schema sales;
-grant usage on schema sales to Aryan;
-CREATE SCHEMA
-GRANT
-Q9 revoke usage on schema sales from Aryan, Aalekh, Aman;
-REVOKE
-Q10 grant connect on database company to Aryan;
-GRANT
-=======
  public | employees   | table | postgres
 (2 rows)
 
@@ -111,7 +75,3 @@ REVOKE
 
 Q10. grant connect on database company to Aalekh;
 GRANT
-<<<<<<< HEAD
->>>>>>> DCL
-=======
->>>>>>> DCL
